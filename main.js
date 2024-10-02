701ac673973cd9bfb456b2235b4277a237034cc5
sbknk <136720653+sbkinoko@users.noreply.github.com>
2024-10-03 00:22:29 +0900

    参考サイトに合わせた

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-function writeHeader() {
-    $("#header").load("header.html");
+function writeHeader(rootDir) {
+    $.ajax({
+        url: rootDir + "header.html", //パスはcommon.jsが読み込まれたHTMLファイルが基準
+        cache: false, //キャッシュを利用するか（お好みで）
+        async: true, //非同期で読み込むか（お好みで）
+        success: function(html){
+            html = html.replace(/\{\$root\}/g, rootDir);
+            document.write(html);
+        }
+    });
 }
