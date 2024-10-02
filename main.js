f05da793d623e4ab7a6dc25677a273b02e2fdef8
sbknk <136720653+sbkinoko@users.noreply.github.com>
2024-10-03 00:37:30 +0900

    js変更

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
 function writeHeader(rootDir) {
-    $.ajax({
-        url: rootDir + "header.html",
-        cache: false, 
-        async: true,
-        success: function(html){
-            html = html.replace(/\{\$root\}/g, rootDir);
-            document.write(html);
-        }
-    });
+    $("#header").load("header.html");
 }
