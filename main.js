2c0dca2c6dad7ca2567da0241b297cd515314e1b
sbknk <136720653+sbkinoko@users.noreply.github.com>
2024-10-03 00:11:05 +0900

    コード修正

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-function writeHeader(rootDir){
-    $.ajax({
-        url: "header.html", 
-        cache: false, 
-        async: false, 
-        success: function(html){
-            html = html.replace(/\{\$root\}/g, rootDir);
-            document.write(html);
-        }
-    });
+function writeHeader() {
+    $("#header").load("header.html");
 }
