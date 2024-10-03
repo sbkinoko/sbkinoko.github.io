ebd717a43ecd3c69dbeee4a3801d4a1621c42df4
sbknk <136720653+sbkinoko@users.noreply.github.com>
2024-10-03 22:10:56 +0900

    ページ分割

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 function writeHeader(rootDir) {
     $("#header").load("header.html");
 }
+
+function writeAbout(){
+    $("#about").load(rootDir+"home/about.html")
+}
+
+function writeArticles(rootDir){
+    $("#articles").load(rootDir+"home/articles.html")
+}
+
+function writeWant(rootDir){
+    $("#want").load(rootDir+"home/articles.html")
+}
+
+function writeWant(rootDir){
+    $("#habitat").load(rootDir+"home/habitat.html")
+}
